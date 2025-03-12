e16c907e84855d97dcaab361dc82f239c248d6a9
Emmanuella Budu <emmabudu@gmail.com>
2025-03-12 17:12:39 +0100

    Update tlstm.py

--- models/tlstm.py
+++ models/tlstm.py
@@ -10,103 +10,103 @@ import numpy as np
 ## from https://github.com/duskybomb/tlstm/blob/master/tlstm.py
 ## https://github.com/yinchangchang/CAT-LSTM/blob/main/code/model.py
 class TLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, cuda_flag=True):
+    def __init__(self, input_size, hidden_size):
         super(TLSTM, self).__init__()
         self.hidden_size = hidden_size
-        self.input_size = input_size
-        self.cuda_flag = cuda_flag
         
-        
-        self.Wi = nn.Linear(input_size, hidden_size)
-        self.Ui = nn.Linear(hidden_size, hidden_size)
-        self.bi = nn.Parameter(torch.zeros(hidden_size))
+        self.Wi = nn.Linear(input_size, hidden_size, bias=True)
+        self.Ui = nn.Linear(hidden_size, hidden_size, bias=False)
 
-        self.Wf = nn.Linear(input_size, hidden_size)
-        self.Uf = nn.Linear(hidden_size, hidden_size)
-        self.bf = nn.Parameter(torch.zeros(hidden_size))
+        self.Wf = nn.Linear(input_size, hidden_size, bias=True)
+        self.Uf = nn.Linear(hidden_size, hidden_size, bias=False)
 
-        self.Wog = nn.Linear(input_size, hidden_size)
-        self.Uog = nn.Linear(hidden_size, hidden_size)
-        self.bog = nn.Parameter(torch.zeros(hidden_size))
+        self.Wo = nn.Linear(input_size, hidden_size, bias=True)
+        self.Uo = nn.Linear(hidden_size, hidden_size, bias=False)
 
-        self.Wc = nn.Linear(input_size, hidden_size)
-        self.Uc = nn.Linear(hidden_size, hidden_size)
-        self.bc = nn.Parameter(torch.zeros(hidden_size))
+        self.Wc = nn.Linear(input_size, hidden_size, bias=True)
+        self.Uc = nn.Linear(hidden_size, hidden_size, bias=False)
 
-       
         self.W_decomp = nn.Linear(hidden_size, hidden_size)
-        self.b_decomp = nn.Parameter(torch.zeros(hidden_size))
+        
+        
+        self.W_t = nn.Linear(1, 1)
+        self.b_t = nn.Parameter(torch.zeros(1))
+
 
     def map_elapse_time(self, t):
-    
-        return 1 / torch.log(t + 2.7183) #epsilon value from Baytas implementation 
+        return 1 / torch.log(t + 2.7183+ 1e-6)
 
-    def forward(self, inputs, timestamps, initial_states=None):
-        b, seq, embed = inputs.size()
-        if initial_states is None:
-            h = torch.zeros(b, self.hidden_size).to(inputs.device)
-            c = torch.zeros(b, self.hidden_size).to(inputs.device)
+    def forward(self, inputs, timestamps, hx=None):
+        batch_size, seq_len, _ = inputs.size()
+
+        if hx is None:
+            h_t = torch.zeros(batch_size, self.hidden_size, device=inputs.device)
+            c_t = torch.zeros(batch_size, self.hidden_size, device=inputs.device)
         else:
-            h, c = initial_states
-        
-        outputs = []
-        hidden_states = []  
-        cell_states = []   
-        
-        for s in range(seq):
-            x = inputs[:, s, :]
-            time_t = timestamps[:, s:s + 1]
-            T = self.map_elapse_time(time_t)
-            
-           
-            C_ST = torch.tanh(self.W_decomp(c) + self.b_decomp)
-            C_ST_dis = T * C_ST
-            c = c - C_ST + C_ST_dis  
+            h_t, c_t = hx
 
-           
-            i = torch.sigmoid(self.Wi(x) + self.Ui(h) + self.bi)
-            f = torch.sigmoid(self.Wf(x) + self.Uf(h) + self.bf)
-            o = torch.sigmoid(self.Wog(x) + self.Uog(h) + self.bog)
-            C = torch.tanh(self.Wc(x) + self.Uc(h) + self.bc)
-            c = f * c + i * C  
-            h = o * torch.tanh(c)  
-            
-            outputs.append(h)
-            hidden_states.append(h)  
-            cell_states.append(c)    
+        outputs = []
         
-       
-        outputs = torch.stack(outputs, 1)
-        hidden_states = torch.stack(hidden_states, 1)  
-        cell_states = torch.stack(cell_states, 1)      
-        return outputs, (hidden_states, cell_states)
+        for t in range(seq_len):
+            x_t = inputs[:, t, :]
+            time_t = timestamps[:, t].unsqueeze(1)  
+            T = self.map_elapse_time(time_t) 
+
+            C_ST = torch.tanh(self.W_decomp(c_t))  
+            C_ST_dis = T * C_ST  
+            c_t = c_t - C_ST + C_ST_dis 
+
+            i_t = torch.sigmoid(self.Wi(x_t) + self.Ui(h_t))
+            f_t = torch.sigmoid(self.Wf(x_t) + self.Uf(h_t))
+            o_t = torch.sigmoid(self.Wo(x_t) + self.Uo(h_t))
+            C_t = torch.tanh(self.Wc(x_t) + self.Uc(h_t))
+            
+            c_t = f_t * c_t + i_t * C_t
+            h_t = o_t * torch.tanh(c_t)
+
+            outputs.append(h_t.unsqueeze(1))
+
+        outputs = torch.cat(outputs, dim=1)
+
+        return outputs, (h_t.unsqueeze(0), c_t.unsqueeze(0))
+
 
 class StackedTLSTM(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, cuda_flag=True):
+    def __init__(self, input_size, hidden_size, num_layers):
+
         super(StackedTLSTM, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.layers = nn.ModuleList([TLSTM(input_size if i == 0 else hidden_size, hidden_size, cuda_flag) for i in range(num_layers)])
-        self.cuda_flag = cuda_flag
 
-    def forward(self, inputs, timestamps, initial_state=None):
-        b, seq, _ = inputs.size()
-        if initial_state is None:
-            initial_states = [
-                (torch.zeros(b, self.layers[i].hidden_size).to(inputs.device),
-                 torch.zeros(b, self.layers[i].hidden_size).to(inputs.device))
-                for i in range(self.num_layers)]
+       
+        self.layers = nn.ModuleList()
+        for i in range(num_layers):
+            layer_input_size = input_size if i == 0 else hidden_size  
+            self.layers.append(TLSTM(layer_input_size, hidden_size))
+
+    def forward(self, inputs, timestamps, hx=None):
+       
+        batch_size, seq_len, _ = inputs.size()
+
+        if hx is None:
+            h_t = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=inputs.device)
+            c_t = torch.zeros(self.num_layers, batch_size, self.hidden_size, device=inputs.device)
         else:
-            initial_states = [initial_state] * self.num_layers
+            h_t, c_t = hx
 
-        curr_input = inputs
-        states = []
-        final_output = None
-        
+        layer_input = inputs  
+        hidden_states = []
+        cell_states = []
+
         for i, layer in enumerate(self.layers):
-            current_state = initial_states[i]
-            output, state = layer(curr_input, timestamps, current_state)
-            final_output = output
-            curr_input = output
-            states.append(state)
-        return final_output, states
+            output, (h_i, c_i) = layer(layer_input, timestamps, (h_t[i], c_t[i]))
+            layer_input = output 
+            hidden_states.append(h_i)
+            cell_states.append(c_i)
+
+      
+        h_n = torch.cat(hidden_states, dim=0)  
+        c_n = torch.cat(cell_states, dim=0) 
+
+        return output, (h_n, c_n)
+
