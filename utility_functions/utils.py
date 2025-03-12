11995cb54c34ebf9c7047b09ec08206e542cdae5
Emmanuella Budu <emmabudu@gmail.com>
2025-03-12 17:32:37 +0100

    Update utils.py

--- utility_functions/utils.py
+++ utility_functions/utils.py
@@ -34,6 +34,56 @@ def rounder(df, cols, dp):
     df_rounded[col] = df_rounded[col].apply(lambda val: np.round(val,dp))
 
   return df_rounded
+  
+def train_epoch(model, dataloader, optimizer, criterion, criterion_time, alpha, beta, gamma, delta):
+    model.train()
+    train_loss = 0
+
+    for batch_idx, (sequences, lengths, time) in enumerate(dataloader):
+        sequences, time = sequences.to(device), time.to(device)
+        optimizer.zero_grad()
+
+        recon_batch, recon_time, mu, logvar, z_real, h = model(sequences, time)
+
+        loss_time = criterion_time(recon_time, time, beta)
+        loss_seq, recons, mmd, sts = criterion(recon_batch, sequences, z_real, mu, logvar, alpha, delta)
+   
+
+        loss = loss_seq + loss_time 
+        loss.backward()
+
+        train_loss += loss.item()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+        optimizer.step()
+
+    return train_loss / len(dataloader), mu, logvar, z_real
+
+
+def validate_epoch(model, val_loader, criterion, criterion_time, alpha, beta, gamma, delta):
+    model.eval()
+    total_loss = 0
+    total_time_loss, total_recon_loss, total_mmd_loss,  total_sts_loss = 0, 0, 0, 0, 
+
+    with torch.no_grad():
+        for batch_idx, (sequences, lengths, time) in enumerate(val_loader):
+            sequences, time = sequences.to(device), time.to(device)
+            recon_batch, recon_time, mu, logvar, z_val, h = model(sequences, time)
+
+            loss_time = criterion_time(recon_time, time, beta)
+            loss_seq, recons, mmd, sts = criterion(recon_batch, sequences, z_val, mu, logvar, alpha, delta)
+ 
+
+            loss = loss_seq + loss_time 
+            total_loss += loss.item()
+            total_time_loss += loss_time.item()
+            total_recon_loss += recons.item()
+            total_mmd_loss += mmd.item()
+           
+            total_sts_loss += sts.item()
+
+    num_batches = len(val_loader)
+    return (total_loss / num_batches, total_time_loss / num_batches, total_recon_loss / num_batches, 
+            total_mmd_loss / num_batches,  total_sts_loss / num_batches)
 
 def sample_from_vae(vae_model, num_samples, batch_size, seq_len, device):
     vae_model.to(device)
