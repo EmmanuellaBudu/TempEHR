74ac267aaecc3826fd786fbc5a61cb95f6efe758
Emmanuella Budu <emmabudu@gmail.com>
2025-03-16 22:08:39 +0100

    Update losses.py

--- models/losses.py
+++ models/losses.py
@@ -18,30 +18,16 @@ def compute_mmd(x, y):
     mmd = x_kernel.mean() + y_kernel.mean() - 2*xy_kernel.mean()
     return mmd
 
-# Used in generative models to match the mean and variance of the real data
-def stats_loss(X, X_hat):
-    X_mean, X_var = torch.mean(X, dim=1), torch.var(X, dim=1)
-    X_hat_mean, X_hat_var = torch.mean(X_hat, dim=1), torch.var(X_hat, dim=1)
-
-    G_loss_V1 = torch.mean(torch.abs(torch.sqrt(X_hat_var + 1e-6) - torch.sqrt(X_var + 1e-6)))
-    G_loss_V2 = torch.mean(torch.abs(X_hat_mean - X_mean))
-
-    G_loss_V = G_loss_V1 + G_loss_V2
-
-    return G_loss_V
-
 class MMDLoss(nn.Module):
     def __init__(self):
         super(MMDLoss, self).__init__()
 
     def forward(self,  x_recon, x, z, mu,logvar,alpha, delta):
         recons_loss= F.mse_loss(x_recon, x)*delta
-        st_loss=stats_loss(x, x_recon)*delta
-  
         z_prior= torch.randn_like(z)
         mmd_loss= compute_mmd(z,z_prior)*alpha
-        loss = recons_loss+mmd_loss+st_loss
-        return loss, recons_loss, mmd_loss, st_loss
+        loss = recons_loss+mmd_loss
+        return loss, recons_loss, mmd_loss
     
 class TimeLoss(nn.Module):
     def __init__(self):
